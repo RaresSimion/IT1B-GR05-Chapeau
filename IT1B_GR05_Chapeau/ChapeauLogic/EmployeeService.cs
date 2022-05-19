ab5d25362c3411cbc8ce126c7c4d8ce96fd595f4
RaresSimion <683655@student.inholland.nl>
2022-05-19 22:10:19 +0200

    Order view

---
+++ IT1B_GR05_Chapeau/ChapeauLogic/EmployeeService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ChapeauDAL;
+using ChapeauModel;
+using System.Collections.Generic;
+
+namespace ChapeauLogic
+{
+    public class EmployeeService
+    {
+        EmployeeDAO employeedatabase;
+
+        public EmployeeService()
+        {
+            employeedatabase = new EmployeeDAO();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employeesList = new List<Employee>();
+            return employeesList;
+        }
+
+        //public Employee GetEmployeeByPassword(string password)
+        //{
+        //    Employee employee = employeedatabase.GetEmployeeByPassword(password);
+        //    return employee;
+        //}
+    }
+}
---
+++ IT1B_GR05_Chapeau/ChapeauLogic/TableService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ChapeauDAL;
+using ChapeauModel;
+using System.Collections.Generic;
+
+
+namespace ChapeauLogic
+{
+    public class TableService
+    {
+        TableDAO tabledatabase;
+
+        public TableService()
+        {
+            tabledatabase = new TableDAO();
+        }
+
+        //public Table GetTable(int tablenr)
+        //{
+        //    Table table = tabledatabase.GetTable(tablenr);
+        //    return table;
+        //}
+
+        public List<Table> GetTables()
+        {
+            List<Table> tables = tabledatabase.GetAllTables();
+            return tables;
+        }
+
+        public void UpdateTable(Table table)
+        {
+            tabledatabase.UpdateTable(table);
+        }
+    }
+}
