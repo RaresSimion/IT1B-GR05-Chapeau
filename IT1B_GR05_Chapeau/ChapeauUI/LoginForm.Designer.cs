a56d9173536c5247c42514861dae34cb25965363
Conor <667496@student.inholland.nl>
2022-05-20 00:40:02 +0200

    login screen

---
+++ IT1B_GR05_Chapeau/ChapeauUI/LoginForm.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LoginScreenMark2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelExit = new System.Windows.Forms.Label();
+            this.labelCLear = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChapeauUI.Properties.Resources.Chapeau_original;
+            this.pictureBox1.Location = new System.Drawing.Point(99, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 138);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelWelcome.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelWelcome.Location = new System.Drawing.Point(127, 153);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(351, 45);
+            this.labelWelcome.TabIndex = 1;
+            this.labelWelcome.Text = "Welcome To Chapeau!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(47, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(491, 45);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please Enter Your Details Below";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ChapeauUI.Properties.Resources.png_transparent_profile_logo_computer_icons_user_user_blue_heroes_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 320);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ChapeauUI.Properties.Resources.icon;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 380);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUser.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxUser.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.textBoxUser.Location = new System.Drawing.Point(127, 320);
+            this.textBoxUser.Multiline = true;
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(362, 50);
+            this.textBoxUser.TabIndex = 5;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPass.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPass.ForeColor = System.Drawing.Color.SeaGreen;
+            this.textBoxPass.Location = new System.Drawing.Point(127, 380);
+            this.textBoxPass.Multiline = true;
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(362, 50);
+            this.textBoxPass.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(216, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 53);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "LOG IN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelExit
+            // 
+            this.labelExit.AutoSize = true;
+            this.labelExit.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelExit.ForeColor = System.Drawing.Color.IndianRed;
+            this.labelExit.Location = new System.Drawing.Point(251, 512);
+            this.labelExit.Name = "labelExit";
+            this.labelExit.Size = new System.Drawing.Size(67, 40);
+            this.labelExit.TabIndex = 8;
+            this.labelExit.Text = "Exit";
+            this.labelExit.Click += new System.EventHandler(this.labelExit_Click);
+            // 
+            // labelCLear
+            // 
+            this.labelCLear.AutoSize = true;
+            this.labelCLear.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelCLear.ForeColor = System.Drawing.Color.IndianRed;
+            this.labelCLear.Location = new System.Drawing.Point(415, 427);
+            this.labelCLear.Name = "labelCLear";
+            this.labelCLear.Size = new System.Drawing.Size(168, 32);
+            this.labelCLear.TabIndex = 9;
+            this.labelCLear.Text = "*Clear Details";
+            this.labelCLear.Click += new System.EventHandler(this.labelCLear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.labelCLear);
+            this.Controls.Add(this.labelExit);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelWelcome);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelExit;
+        private System.Windows.Forms.Label labelCLear;
+    }
+}
