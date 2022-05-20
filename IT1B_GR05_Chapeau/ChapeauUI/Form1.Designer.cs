df5337373012d72de2e6b993f962f509359e2a04
Jojoelo12 <89935806+Jojoelo12@users.noreply.github.com>
2022-05-20 12:13:21 +0200

    close

--- IT1B_GR05_Chapeau/ChapeauUI/Form1.Designer.cs
+++ IT1B_GR05_Chapeau/ChapeauUI/Form1.Designer.cs
@@ -470,7 +470,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.listViewOrder);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
